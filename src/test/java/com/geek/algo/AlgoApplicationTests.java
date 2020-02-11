692409cf92e19b3ca28ca964d1262964378633a0
duyaoguang <duyaoguang@vipkid.com.cn>
2020-02-11 10:44:26 +0800

    feat(first commit): first commit

---
+++ src/test/java/com/geek/algo/AlgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geek.algo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
